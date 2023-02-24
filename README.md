94021cc2b3dce1402af0a592d4dff9babb756e0c
martiolozz <martiolitozz@gmail.com>
2023-02-24 01:46:24 -0500

    configure enfura endpoint,set config to export in goerli network

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
+Deploying contracts with account:  0xaFF63138A5ec37f3b36bF5C83b8FC639E2857942
+Account balance:  110000000000000000
+WavePortal address:  0x582fe3dEFDCb59F797fcE5ed0839E211D6c02f8b
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -17,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
     solidity: "0.8.17",
+    networks: {
+      goerli: {
+        url: "https://goerli.infura.io/v3/d7d55ab3c2d0480fb55e67b6def4cfe1",
+        accounts: ["9da88673a23891fc7d7eb93f15b842117f74f36f522d7337d58928e661d10c31"]
+      },
+    },
 };
